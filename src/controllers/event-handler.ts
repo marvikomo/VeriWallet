f8669a828dcdeac4f19e5a36ab57cbb730d8175c
marvikomo <marvikomo@gmail.com>
2024-04-15 00:32:37 +0100

    implemented event-handler

---
+++ src/controllers/event-handler.ts
@@ -0,0 +1,33 @@
+import { v4 as uuid } from 'uuid';
+
+export const extensionInstances = {};
+export const providerInstances = {};
+
+export const handleEvent = (port, controller) => {
+  const id = uuid()
+  //To prevent cross site
+  if (port.name !== 'provider') {
+    throw new Error('Unknow origin')
+  }
+
+  const url = new URL(port?.sender?.url)
+  providerInstances[id] = {
+    port,
+    tabId: port?.sender?.tab?.id,
+    windowId: port?.sender?.tab?.windowId,
+    origin: url.origin,
+    urlHostName: url.hostname,
+  }
+
+  const messageListener = (message: any, port: any) => {
+    controller.handler(message, port, id);
+  }
+
+  port.onMessage.addListener(messageListener);
+
+  port.onDisconnect.addListener((port) => {
+    port.onMessage.removeListener(messageListener);
+    delete providerInstances[id];
+  })
+
+}
